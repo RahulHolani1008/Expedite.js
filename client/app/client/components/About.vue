38b9f90b0e7fac28fda6b3554bf895c49ad45734
rahul-hunterdigitaldev <rahulh@hunter.digital>
2021-02-07 21:20:30 +0530

    Added SSR client

---
+++ client/app/client/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "About page",
+  },
+  serverPrefetch() {
+    console.log("Run only on server 2");
+  },
+};
+</script>
---
+++ client/app/client/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="q-app">
+    <div class="q-ma-md">
+      Fork and make your own! Do NOT use self-closing tags here on Codepen.
+    </div>
+
+    <div class="q-ma-md">
+      <q-btn label="Notify" color="primary" @click="notify"></q-btn>
+    </div>
+
+    <div class="q-ma-xl">Running Quasar v{{ version }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Expedite",
+  },
+  data() {
+    return {
+      title: "Default title",
+      version: "1",
+    };
+  },
+  mounted() {
+    console.log("Mounted");
+  },
+  serverPrefetch() {
+    console.log("Run only on server");
+  },
+  methods: {
+    notify: function () {
+      this.$q.notify("Running on Quasar v" + this.$q.version);
+    },
+  },
+};
+</script>
